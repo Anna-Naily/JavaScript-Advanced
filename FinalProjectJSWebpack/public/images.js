be290ad95adede93d4d33ac7202b3396722bdf8e
Anna_Umanskaya <zareth2_@mail.ru>
2021-09-12 18:17:44 +0300

    final project

---
+++ FinalProjectJSWebpack/public/images.js
@@ -0,0 +1,53 @@
+import product_1 from "./image/product-card-1.png";
+import product_2 from "./image/product-card-2.png";
+import product_3 from "./image/product-card-3.png";
+import product_4 from "./image/product-card-4.png";
+import product_5 from "./image/product-card-5.png";
+import product_6 from "./image/product-card-6.png";
+import product_7 from "./image/product-card-7.png";
+import product_8 from "./image/product-card-8.png";
+import product_9 from "./image/product-card-9.png";
+import product_10 from "./image/product-card-10.png";
+import product_11 from "./image/product-card-11.png";
+import logo from "./image/logo.png";
+import avatar from "./image/avatar.png";
+import light_line from "./image/line-slider-light.png";
+import pink_line from "./image/line-slider-pink.png";
+import promo from "./image/Promo.png";
+import line_single from "./image/line-single.png";
+import logo_cart from "./image/logo_cart.svg";
+import white_cart from "./image/white-cart.svg";
+import delivery from "./image/delivery.svg";
+import discount from "./image/discount.svg";
+import crown from "./image/crown.svg";
+import pink_logo_cart from "./image/pink-logo-cart.svg";
+
+const images = {
+  components: {
+    product_1,
+    product_2,
+    product_3,
+    product_4,
+    product_5,
+    product_6,
+    product_7,
+    product_8,
+    product_9,
+    product_10,
+    product_11,
+    logo,
+    logo_cart,
+    white_cart,
+    delivery,
+    discount,
+    crown,
+    avatar,
+    light_line,
+    pink_line,
+    promo,
+    line_single,
+    pink_logo_cart,
+  },
+};
+
+export default images;
